97e8183913c0d5e25b7f56e91e319c42d76e6caf
Zobaer <zzobaer09@gmail.com>
2022-10-23 18:14:42 +0600

    signup added

--- register/views.py
+++ register/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import SignUpForm
 # Create your views here.
+
+def register(response):
+    if response.method == "POST":
+        form = SignUpForm(response.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    else:
+        form = SignUpForm()
+    return render(response , "register/register.html", {"signup_form":form})
