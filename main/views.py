972fe2b7f46000663774a540040d1787af4a4dcb
Zobaer <zzobaer09@gmail.com>
2022-10-23 14:22:06 +0600

    simple config

--- main/views.py
+++ main/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect , HttpResponse
 
 # Create your views here.
+
+
+def Home(responser):
+    return render(responser , "main/home.html")
